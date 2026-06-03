f961a9210941642aa76398e59bc3aaf3cfe3286b
Hylmi <hylmig7@gmail.com>
2026-06-03 09:40:02 +0700

    fix: correct Gate facade import

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,7 @@
 
 namespace App\Providers;
 
-use Illuminate\support\Facades\Gate;
+use Illuminate\Support\Facades\Gate;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
